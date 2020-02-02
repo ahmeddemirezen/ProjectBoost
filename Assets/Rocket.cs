b4239633a1607d6a95cddf56404c3b55f2882f92
Ahmed Demirezen <ahmeddemirezen@outlook.com>
2020-02-03 00:21:17 +0300

    ProcessInput function is added.

---
+++ Assets/Rocket.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            print("Thrusting");
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            print("Rotating Left");
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            print("Rotating Right");
+        }
+    }
+}
