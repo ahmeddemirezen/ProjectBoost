af6e6b3e27b6ceb0c64a5df5e0b48d10f5d58ec8
Ahmed Demirezen <ahmeddemirezen@outlook.com>
2020-02-05 00:00:00 +0300

    Fuel script is added. Rocket.cs and FuelScripts.cs are added collusion component.

---
+++ Assets/Scripts/FuelScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuelScript : MonoBehaviour
+{
+    //rotation time
+    float rotTimer=0.0f;
+    float rotWait=0.0005f;
+    float visualTime=0.0f;
+
+    //destroy time
+    float destroyTimer=0.0f;
+    float destroyWait=0.01f;
+    
+    //destroy check variable
+    bool destroyCheck=false;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        rotTimer=Time.deltaTime;
+        destroyTimer=Time.deltaTime;
+        if(destroyTimer>destroyWait)
+            {
+                destroyTimer=visualTime;
+                destroyTimer-=destroyWait;
+                if(destroyCheck==true){
+                    Destroy(gameObject);}
+            }
+        if(rotTimer>rotWait)
+        {
+            rotTimer=visualTime;
+            rotTimer-=rotWait;
+            transform.Rotate(0.0f,1.0f,0.0f);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch(collision.gameObject.tag)
+        {
+            case "Rocket":
+                destroyCheck=true;
+                break;
+            default:
+                break;        
+        }
+    }
+}
---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    //component variables
+    AudioSource audioSource;
+    Rigidbody rigidBody;
+
+    //rocket pyhsics
+    float rocketMass=80.0f;
+    float fuelMass=120.0f;
+
+    //Timer for rotation
+    private float rotTimer= 0.0f;
+    [SerializeField] float rotWaitTime=0.005f;
+
+    //timer for location(thrust)
+    private float locTimer=0.0f;
+    [SerializeField] float locWaitTime=0.05f;
+    private float visualTime=0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody=GetComponent<Rigidbody>();
+        audioSource=GetComponent<AudioSource>();
+        rigidBody.mass=rocketMass+fuelMass;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+        Thrust();
+    }
+    
+    //rotate is changed rocket rotation
+    void Rotate()
+    {
+        rotTimer += Time.deltaTime;//set current frame time
+        if (rotTimer > rotWaitTime)
+        {
+            rotTimer = visualTime;
+            rotTimer -= rotWaitTime;
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(Vector3.forward*2);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(-2*Vector3.forward);
+            }
+        }
+    }
+    
+    //thrust is changed rocket location
+    void Thrust()
+    {
+        locTimer += Time.deltaTime;//set current frame time
+        if (locTimer > locWaitTime)
+        {
+            locTimer = visualTime;
+            locTimer -= locWaitTime;
+            if (Input.GetKey(KeyCode.Space))
+            {
+                if (fuelMass > 0)
+                {
+                    rigidBody.AddRelativeForce(0.0f, 11000.0f, 0.0f);
+                    AudioController("play");
+                    SettingRocketMass();
+                    //print(rigidBody.mass);
+                }
+            }
+            else
+            {
+                AudioController("stop");
+            }
+        }
+    }
+    
+    //mass calculation for rocket pyhsics
+    void SettingRocketMass()
+    {
+        fuelMass-=1.0f;
+        rigidBody.mass=rocketMass+fuelMass;
+    }
+    
+    //thursting audio control
+    void AudioController(string audioFuncCheck)
+    {
+        if(audioFuncCheck=="play")
+        {
+            if(!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else if(audioFuncCheck=="stop")
+        {
+            audioSource.Stop();
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch(collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK");//todo delete
+                break;
+            case "Fuel":
+                print("Fuel");//todo delete
+                break;
+            default:
+                print("Dead");//todo delete
+                break;        
+        }
+    }
+}
